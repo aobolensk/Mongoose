4a5a147aea89eb76d8ae69c4ff51f9698711ba47
Arseniy Obolenskiy <gooddoog@student.su>
2020-04-04 19:42:26 +0300

    Add coarsening example

---
+++ Executable/coarsening.cpp
@@ -0,0 +1,61 @@
+#include "Mongoose_Coarsening.hpp"
+#include "Mongoose_IO.hpp"
+#include "Mongoose_Logger.hpp"
+
+using namespace Mongoose;
+
+int main(int argn, const char **argv)
+{
+    SuiteSparse_start();
+
+    // Set Logger to report only Error messages
+    Logger::setDebugLevel(Error);
+
+    // Turn timing information on
+    Logger::setTimingFlag(true);
+
+    if (argn < 2 || argn > 2)
+    {
+        // Wrong number of arguments - return error
+        LogError("Usage: coarsening <MM-input-file.mtx> [output-file]");
+        return EXIT_FAILURE;
+    }
+
+    // Read in input file name
+    std::string inputFile = std::string(argv[1]);
+
+    EdgeCut_Options *options = EdgeCut_Options::create();
+    if (!options)
+    {
+        // Ran out of memory
+        LogError("Error creating Options struct");
+        return EXIT_FAILURE;
+    }
+
+    Graph *graph = read_graph(inputFile);
+
+    if (!graph)
+    {
+        // Ran out of memory or problem reading the graph from file
+        LogError("Error reading Graph from file");
+        return EXIT_FAILURE;
+    }
+
+    EdgeCutProblem *ecp = EdgeCutProblem::create(graph);
+    if (!graph)
+    {
+        LogError("Error creating EdgeCutProblem");
+        return EXIT_FAILURE;
+    }
+
+    match(ecp, options);
+    EdgeCutProblem *result = coarsen(ecp, options);
+    if (!result)
+    {
+        LogError("Error coarsening");
+        return EXIT_FAILURE;
+    }
+
+    write_graph(result);
+    return EXIT_SUCCESS;
+}
